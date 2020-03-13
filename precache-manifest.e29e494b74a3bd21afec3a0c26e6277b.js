64bfcd8f1a661da21348e631f164dea71da3f941
Elbert Ballo <eballofullstackdev@gmail.com>
2020-03-12 22:07:08 -0700

    Updates

---
+++ precache-manifest.e29e494b74a3bd21afec3a0c26e6277b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda1676e08c0d43948bf0d835f3d7747",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d5467a41808f4cc87a6d",
+    "url": "/monsters-rolodex/static/css/main.150f99d8.chunk.css"
+  },
+  {
+    "revision": "cadec4fa40fa498dc1c8",
+    "url": "/monsters-rolodex/static/js/2.5f26f679.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.5f26f679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5467a41808f4cc87a6d",
+    "url": "/monsters-rolodex/static/js/main.fcc62530.chunk.js"
+  },
+  {
+    "revision": "ffb94150b2ca4d489ead",
+    "url": "/monsters-rolodex/static/js/runtime-main.6d2b0cb8.js"
+  }
+]);
